a1cd0d36bbffd9dda16dcd2c613d399317206579
hashem-buzer <hbuzer98@gmail.com>
2020-01-11 00:09:11 +0100

    validating, bubbles page, index done

--- pages/bubbleCard.js
+++ pages/bubbleCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Row, Col, Card, Container } from "react-bootstrap";
+import { Row, Col, Card, Container, Label } from "react-bootstrap";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
 import Data from "../dummyData.js";
 import Axios from "axios";
+import Link from "next/link";
 
 const BubbleCard = () => (
   <div>
@@ -20,43 +21,76 @@ const BubbleCard = () => (
     >
       bubble card page
     </h2>
-    <Container>
+    <Container style={{ textAlign: "center" }}>
       <Row>
         {Data.map((elem, i) => {
-          if (elem.user.id === "1")
-            return (
-              <Col lg="4">
+          console.log(elem.user.id);
+          if (elem.user.id === "2") {
+            if (elem.user.bubbles.length > 1) {
+              return (
+                <Col lg="8" key={i}>
+                  <div
+                    style={{
+                      margin: "20px"
+                    }}
+                  >
+                    {elem.user.bubbles.map((bubble, i) => {
+                      return (
+                        <Link href="" key={i}>
+                          <Card
+                            className="bubbleCard"
+                            style={{
+                              cursor: "pointer",
+                              border: "solid",
+                              borderRadius: "40%",
+                              marginBottom: "10%",
+                              paddingBottom: "70%",
+                              textAlign: "center",
+                              backgroundImage:
+                                "url(http://getwallpapers.com/wallpaper/full/0/e/7/476297.jpg)",
+                              color: "white",
+                              marginLeft: "30%"
+                            }}
+                          >
+                            {bubble.bubbleName}
+                          </Card>
+                        </Link>
+                      );
+                    })}
+                  </div>
+                </Col>
+              );
+            } else {
+              return (
                 <div
-                  key={i}
+                  id="bubblesCreation"
                   style={{
-                    margin: "20px"
+                    width: "70%",
+                    borderRadius: "40%",
+                    paddingBottom: "30%",
+                    textAlign: "center",
+                    backgroundColor: "red",
+                    color: "white"
                   }}
                 >
-                  {elem.user.bubbles.map((bubble, i) => {
-                    return (
-                      <a href="">
-                        <Card
-                          className="bubbleCard"
-                          key={i}
-                          style={{
-                            border: "solid",
-                            borderRadius: "40%",
-                            marginBottom: "30%",
-                            paddingBottom: "70%",
-                            textAlign: "center",
-                            backgroundImage:
-                              "url(http://getwallpapers.com/wallpaper/full/0/e/7/476297.jpg)",
-                            color: "white"
-                          }}
-                        >
-                          {bubble.bubbleName}
-                        </Card>
-                      </a>
-                    );
-                  })}
+                  Create Your Own Bubble
+                  <form
+                    style={{
+                      textAlign: "center",
+                      marginTop: "30%",
+                      backgroundColor: "green"
+                    }}
+                  >
+                    <label>Bubble's Name:</label>
+                    <input></input>
+                    <br />
+                    <label>Add Members:</label>
+                    <button>generate link</button>
+                  </form>
                 </div>
-              </Col>
-            );
+              );
+            }
+          }
         })}
       </Row>
     </Container>
